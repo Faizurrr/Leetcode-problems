09b35ca3de99341b42ce682ce86ce196a6252e8d
Faizurrahman <faizurbtech@gmail.com>
2025-11-22 21:37:06 +0530

    Time: 0 ms (100%), Space: 23.3 MB (10.78%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-  bool isdivisble(vector<int>nums) {
-    for(int i =0 ; i<nums.size();i++) {
-        if(nums[i]%3!=0) {
-            return false;
-        }
-    }
-     return true;
-  }
-  // remender will in range of{ 0 to 2}
+   // very simple algorithms :
+ // “For any number, if it is not divisible by 3, then either adding 1 or subtracting 1 will make it divisible by 3.”
     int minimumOperations(vector<int>& nums) {
          int count =0;
-         while(!isdivisble(nums)){
+     
         for(int i =0 ; i<nums.size();i++) {
-            if(nums[i]%3==1 && nums[i]!=1){ // 
-             nums[i]-=1;
+            if(nums[i]%3!=0){  
              count++;
-            } else if(nums[i]%3==2 && nums[i]!=2) {
-                nums[i] = nums[i]+1;
-                count+=1;
-            } else if(nums[i]==1) {
-                nums[i]-=1;
-                count++;
-            } else if(nums[i]==2) {
-                nums[i]+=1;
-                count++;
-            }
-        }
+            } 
         }
          return count;
     }
