fd743af3621c6768b28ad3803f25dcd8be65820f
Faizurrahman <faizurbtech@gmail.com>
2025-12-17 23:45:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (49.66%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(long long n) {
+        if (n == 1) return 0;
+
+        if (n % 2 == 0) {
+            return 1 + solve(n / 2);
+        } else {
+            // odd case
+            if (n == 3) {
+                return 2;  
+            }
+            return 1 + min(
+                solve(n - 1),
+                solve(n + 1)
+            );
+        }
+    }
+
+    int integerReplacement(int n) {
+        return solve((long long)n);
+    }
+};
