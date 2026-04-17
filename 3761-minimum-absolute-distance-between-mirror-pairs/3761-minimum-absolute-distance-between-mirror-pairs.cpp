d4c1a2108ba02d622833c94621183409eb26c6ca
Faizurrahman <faizurbtech@gmail.com>
2026-04-17 21:23:48 +0530

    Time: 117 ms (56.86%), Space: 131.9 MB (75.49%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,3 +1,45 @@
+  // Brute force apporoch ....
+
+//  class Solution {
+//     public:
+    
+//           int reversed(int num){
+//         int reversedval = 0;
+//         while (num != 0) {
+//             int lastdigit = num % 10;
+//             reversedval = reversedval * 10 + lastdigit;
+//             num /= 10;
+//         }
+//         return reversedval;
+//     }
+       
+//      int minMirrorPairDistance(vector<int>& nums) {
+//               int minDist = INT_MAX; 
+//             for(int i = 0 ;i <nums.size()-1; i++){
+//                  int rev = reversed(nums[i]);
+//                 int dist = 0 ;
+//                  for(int j = i+1 ; j<nums.size(); j++){
+//                        if( rev == nums[j]) {
+//                              dist = j-i;
+//                               minDist = min(dist , minDist);
+//                              break;
+//                        } 
+                        
+                         
+                       
+//                  }
+//             }
+//              if(minDist == INT_MAX){
+//                    return -1;
+//              } 
+//              return minDist;
+//       } 
+//  };
+
+
+
+  //optimize apporoch -1.
+
 class Solution {
 public:
     int reversed(int num){
@@ -28,3 +70,37 @@ public:
         return ans == INT_MAX ? -1 : ans;
     }
 };
+  //  optimize apporoch-2
+//   class Solution {
+// public:
+//     int reversed(int num) {
+//         int reversedval = 0;
+//         while (num != 0) {
+//             int lastdigit = num % 10;
+//             reversedval = reversedval * 10 + lastdigit;
+//             num /= 10;
+//         }
+//         return reversedval;
+//     }
+
+//     int minMirrorPairDistance(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+
+     
+//         for (int i = 0; i < nums.size(); i++) {
+//             mp[nums[i]] = i;
+//         }
+
+//         int ans = INT_MAX;
+
+//         for (int j = 0; j < nums.size(); j++) {
+//             int rev = reversed(nums[j]);
+
+//             if (mp.find(rev) != mp.end() && mp[rev] != j) {
+//                 ans = min(ans, abs(j - mp[rev]));
+//             }
+//         }
+
+//         return (ans == INT_MAX) ? -1 : ans;
+//     }
+// };
