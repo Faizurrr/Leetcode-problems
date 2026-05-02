14306abe3354d7f037b028f2b2edf818790ec24a
Faizurrahman <faizurbtech@gmail.com>
2026-05-03 00:20:47 +0530

    Time: 0 ms (100%), Space: 7.6 MB (94.4%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,12 +1,23 @@
-class Solution {
-public:
-    int countOdds(int low, int high) { // Brute force Apporoch.....
-         int ans = 0;
-       for(int i = low ; i<=high ; i++){
-            if(i%2!=0){
-                 ans++;
-            }
-       }
-       return ans;
-    }
-};
+// class Solution {
+// public:
+//     int countOdds(int low, int high) { // Brute force Apporoch.....
+//          int ans = 0;
+//        for(int i = low ; i<=high ; i++){
+//             if(i%2!=0){
+//                  ans++;
+//             }
+//        }
+//        return ans;
+//     }
+// };
+  class Solution {
+    public:
+ int countOdds(int low, int high) { 
+       int totalNum = (high - low)+1 ;
+        int odd = totalNum/2; 
+         if(low%2!=0 && totalNum%2!=0){ // START WITH ODD
+             return odd+1;
+         }
+        return odd;
+ }
+  };
